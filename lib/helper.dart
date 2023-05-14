1b220782c2172bde7345efc185503a9c7dbcc356
Raed-IT <RaedSwanIT@gmail.com>
2023-05-14 10:17:36 +0300

    fix  pagination  mixin

--- lib/helper.dart
+++ lib/helper.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'utilitis/constats.dart';
 export 'package:helper_plugin/mixings/files/image_piker_mixing.dart';
 export 'package:helper_plugin/mixings/api/pagination_mixing.dart';
+export 'package:helper_plugin/mixings/api/api_mixin.dart';
+export 'package:helper_plugin/mixings/api/check_response.dart';
 
 class HelperMadaFlutter {
   static void initial({String? appName, Widget? drawerWidget}) {
