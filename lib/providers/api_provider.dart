49fa911a75eb124883e894a44893655ae489d9bc
Raed-It <raedswanit@gmail.com>
2023-11-03 21:31:12 +0300

    print

--- lib/providers/api_provider.dart
+++ lib/providers/api_provider.dart
@@ -1,12 +1,17 @@
 import 'package:get/get.dart';
+import 'package:helper_plugin/utilitis/helper_functions.dart';
 
 import '../utilitis/base_get_connect.dart';
 
 class ApiProvider extends BaseGetConnect {
-  Future<Response> getData({required String url}) => get(url);
+  Future<Response> getData({required String url}) async {
+    printHelper("get url pagination => $url");
+    return await get(url);
+  }
 
   Future<Response> postData({required String url, required FormData data}) =>
       post(url, data);
-  Future<Response> deleteData({required String url, required int id }) =>
+
+  Future<Response> deleteData({required String url, required int id}) =>
       delete("$url/$id");
 }
